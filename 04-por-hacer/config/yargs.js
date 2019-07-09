8cb2201322c3edc1d0dd9b58a646ae6da8c96033
jdd97 <daniolimpia97@gmail.com>
2019-07-08 20:30:11 -0400

    ok

---
+++ 04-por-hacer/config/yargs.js
@@ -0,0 +1,26 @@
+argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', {
+        descripcion: {
+            demand: true,
+            alias: 'd',
+            desc: 'Descripcion de öa tarea por hacer'
+        }
+    })
+    .command('actualizar', 'Actualiza el estado completo de una tarea', {
+        descripcion: {
+            demand: true,
+            alias: 'd',
+            desc: 'Descripcion de öa tarea por hacer'
+        },
+        completado: {
+            default: true,
+            alias: 'c',
+            desc: 'Marca como completado o pendiente la tarea'
+        }
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
